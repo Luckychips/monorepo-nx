fde793ea9df0cfb18483afe97070048d24ff2c7b
todd.kim <sksms20000@gmail.com>
2022-10-09 21:34:55 +0900

    check import ui component

--- apps/backoffice-web/src/app/app.tsx
+++ apps/backoffice-web/src/app/app.tsx
@@ -1,5 +1,6 @@
 import styled from '@emotion/styled';
 import NxWelcome from './nx-welcome';
+import { UiShared } from '@ui-shared';
 
 const StyledApp = styled.div`
   // Your style here
@@ -9,6 +10,7 @@ export function App() {
   return (
     <StyledApp>
       <NxWelcome title="backoffice-web" />
+      <UiShared />
     </StyledApp>
   );
 }
