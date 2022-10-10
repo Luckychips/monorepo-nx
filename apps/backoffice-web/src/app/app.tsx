0b50e8376196240b66f54f6dc3a1ab95f42eb11c
todd.kim <sksms20000@gmail.com>
2022-10-10 16:08:05 +0900

    add tabler-admin-template from backoffice-web

--- apps/backoffice-web/src/app/app.tsx
+++ apps/backoffice-web/src/app/app.tsx
@@ -1,6 +1,8 @@
 import styled from '@emotion/styled';
-import NxWelcome from './nx-welcome';
+import { Card, Button } from 'tabler-react';
 import { Welcome } from '@ui-shared';
+import NxWelcome from './nx-welcome';
+import "tabler-react/dist/Tabler.css";
 
 const StyledApp = styled.div`
   // Your style here
@@ -10,6 +12,14 @@ export function App() {
   return (
     <StyledApp>
       <NxWelcome title="backoffice-web" />
+      <Card>
+        <Card.Header>
+          <Card.Title>Card Title</Card.Title>
+        </Card.Header>
+        <Card.Body>
+          <Button color="primary">A Button</Button>
+        </Card.Body>
+      </Card>
       <Welcome />
     </StyledApp>
   );
